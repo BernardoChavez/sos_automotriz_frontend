3ae366b4369e6b98a9a30eb91b9bc23bb1a39a42
BernardoChavez <bernardochavez595@gmail.com>
2026-04-12 18:16:31 -0400

    cambios

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,23 @@
-export class AppComponent {
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar'; // Importa tu nuevo sidebar
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, SidebarComponent], // Agrega SidebarComponent aquí
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss']
+})
+export class AppComponent implements OnInit {
   isLoggedIn: boolean = false;
-  currentPageTitle: string = 'Panel de Control';
+  currentPageTitle: string = 'SOS Automotriz';
   user: any = null;
 
-  constructor() {
+  constructor() { }
+
+  ngOnInit() {
     this.checkLogin();
   }
 
